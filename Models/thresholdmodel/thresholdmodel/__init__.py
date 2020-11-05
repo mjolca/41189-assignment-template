40f9248115c5941581f27bcc6d30581ca8a95c48
mjolca <69468367+mjolca@users.noreply.github.com>
2020-11-05 18:33:47 +1100

    Revert "Merge branch 'master' of https://github.com/mjolca/41189-assignment-template"
    This reverts commit f30dd4764a52a75ac279e7394c59910c21ce91cb, reversing
    changes made to 0205ff5136608e69c1ea038a9754e52ecd2abce9.

--- Models/thresholdmodel/thresholdmodel/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-A module to simulate Watts' threshold model
-on static networks (in continuuous time).
-"""
-
-from .metadata import __version__
-from .model import ThreshModel
